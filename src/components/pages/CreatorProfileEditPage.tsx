8e2d876f07c950636615fc3913b8d4e2b70ccdb1
Wataru Ono <wtr0414@gmail.com>
2020-11-08 23:44:18 +0900

    :sparkles: 必要なページをペライチで作成

---
+++ src/components/pages/CreatorProfileEditPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CreatorProfileEditPage: React.FC = () => {
+  return <h1>CreatorProfileEditPage</h1>
+}
+
+export default CreatorProfileEditPage
---
+++ src/components/pages/StarterProfileEditPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const StarterProfileEditPage: React.FC = () => {
+  return <h1>StarterProfileEditPage</h1>
+}
+
+export default StarterProfileEditPage
