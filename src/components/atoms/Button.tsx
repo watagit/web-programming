9ca06f337b8e4662230980d87cc6d4a902b91a94
Wataru Ono <wtr0414@gmail.com>
2020-11-18 11:06:21 +0900

    :sparkles: Buttonを作成

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,29 +1,47 @@
 import React from 'react'
 import styled from 'styled-components'
 
-type ButtonProps = {
-  width?: string,
-  height?: string,
-  text?: string,
-}
-
 const StyledButton = styled.button<
-  Pick<ButtonProps, 'width' | 'height'>
+  Pick<ButtonProps, 'width' | 'height' | 'fontSize' | 'labelColor' | 'fillColor' | 'borderColor'>
   >`
+  border-radius: 5px;
+  font-weight: bold;
   width: ${({ width } ) => width};
   height: ${({ height }) => height};
+  font-size: ${({ fontSize }) => fontSize};
+  color: ${({ labelColor }) => labelColor};
+  background-color: ${({ fillColor }) => fillColor};
+  border: solid 2px ${({ borderColor }) => borderColor};
 `
 
+type ButtonProps = {
+  label: string,
+  width: string,
+  height: string,
+  fontSize: string,
+  labelColor: string,
+  fillColor: string,
+  borderColor: string,
+}
+
 const Button: React.FC<ButtonProps> = ({
+  label,
   width,
   height,
-  text,
-                                       }) => {
+  fontSize,
+  labelColor,
+  fillColor,
+  borderColor
+}) => {
   return (
     <StyledButton
       width={width}
       height={height}
-    >{text}</StyledButton>
+      fontSize={fontSize}
+      labelColor={labelColor}
+      fillColor={fillColor}
+      borderColor={borderColor}
+    >{label}</StyledButton>
   )
 }
 
