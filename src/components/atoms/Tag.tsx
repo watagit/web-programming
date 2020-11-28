5f72d6d41f5ecb4098f89125ba58d993ad9701e3
Wataru Ono <wtr0414@gmail.com>
2020-11-28 22:19:21 +0900

    :wrench: Tag Atomの設計を変更

--- src/components/atoms/Tag.tsx
+++ src/components/atoms/Tag.tsx
@@ -2,27 +2,34 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledTag = styled.div<
-  Pick<TagProps, 'fontSize'>
+  Pick<TagProps, 'fontSize' | 'padding' | 'margin'>
 >`
-  display: inline;
+  display: inline-block;
   background-color: #E2E2E2;
   border-radius: 5px;
-  padding: 10px 20px;
   font-size: ${({ fontSize }) => fontSize};
+  margin: ${({ margin }) => margin};
+  padding: ${({ padding }) => padding};
 `
 
-type TagProps = {
+export type TagProps = {
   label: string,
   fontSize: string,
+  margin: string,
+  padding: string,
 }
 
 const Tag: React.FC<TagProps> = ({
   label,
   fontSize,
+  margin,
+  padding,
 }) => {
   return (
     <StyledTag
       fontSize={fontSize}
+      margin={margin}
+      padding={padding}
     >
       {label}
     </StyledTag>
