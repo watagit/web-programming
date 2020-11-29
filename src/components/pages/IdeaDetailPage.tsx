10baa734e712f442c6bd95f21f0e25ff94738845
Wataru Ono <wtr0414@gmail.com>
2020-11-29 11:17:32 +0900

    :sparkles: 必要な要素を表示

--- src/components/pages/IdeaDetailPage.tsx
+++ src/components/pages/IdeaDetailPage.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
+import FaceIcon from '../../assets/img/face.png'
+import IdeaDetail from '../organisms/IdeaDetail'
+
+const IdeaDetailData = {
+  title: '魔法の絨毯を作りたい',
+  name: '山田太郎',
+  icon: FaceIcon,
+  description: 'descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription',
+  tags: [
+    { label: 'Web', fontSize: '15px', margin: '0 5px 0 0', padding: '7px 10px' },
+    { label: 'ハードウェア', fontSize: '15px', margin: '0 5px 0 0', padding: '7px 10px' },
+  ],
+}
+
 const IdeaDetailPage: React.FC = () => {
-  return <h1>IdeaDetailPage</h1>
+  return (
+    <div>
+      <h1>IdeaDetailPage</h1>
+      <IdeaDetail
+        title={IdeaDetailData.title}
+        name={IdeaDetailData.title}
+        icon={IdeaDetailData.icon}
+        description={IdeaDetailData.description}
+        tags={IdeaDetailData.tags}
+      />
+    </div>
+)
 }
 
 export default IdeaDetailPage
